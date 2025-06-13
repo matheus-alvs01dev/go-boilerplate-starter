0646caf1e50c0201a90345ec694d96f68cf1285d
Matheus Alves <matheus.alvsbarroso@gmail.com>
2025-06-13 12:33:30 -0300

    feat: dockerize

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,18 +1,8 @@
 package main
 
 import (
-	"fmt"
 	_ "github.com/joho/godotenv/autoload"
-	"github.com/matheus-alvs01dev/go-boilerplate/config"
 )
 
 func main() {
-	if err := config.LoadConfig(); err != nil {
-		fmt.Println("Error loading configuration:", err)
-		return
-	}
-
-	fmt.Println("Configuration loaded successfully!")
-	fmt.Println("Server: ", config.GetServerConfig())
-	fmt.Println("Environment: ", config.GetEnv())
 }
